1accb8ae42ca892d20e731fd742156ea9ba69fec
Scutaru Stefan <daniel.stefan02@yahoo.com>
2025-09-20 16:42:02 +0300

    first commit

--- Scraping/scraper OLX.py
+++ Scraping/scraper OLX.py
@@ -13,12 +13,27 @@ for a in anunturi_olx:
     links.append("https://www.olx.ro" + href)
 
 for link in links:
-    response = requests.get(link)
-    soup = BeautifulSoup(response.text, 'lxml')
+    try:
+        response = requests.get(link)
+        soup = BeautifulSoup(response.text, 'lxml')
     
-    judete = soup.find_all('p', class_='css-z0m36u')
-    judet = judete[1].get_text(strip=True) if len(judete) > 1 else None
+        judete = soup.find_all('p', class_='css-z0m36u')
+        judet = judete[1].get_text(strip=True) if len(judete) > 1 else None
+    
+        oras = soup.find('p', class_='css-7wnksb').text.strip()
         
-    pret = soup.find('h3', class_='css-1m6jpd2').text.strip()
-    print(judet)
+        pret = soup.find('h3', class_='css-1m6jpd2').text.strip()
+    
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        continue
+    
+    rezultate.append({
+        'oras': oras,
+        'judet': judet,
+        'pret': pret,})
+
+df = pd.DataFrame(rezultate)
+df.to_csv('olx_imobiliare.csv' , index=False, encoding='utf-8-sig')
+print("Scraping completed and data saved to olx_imobiliare.csv")
  
