c75f880e3aa2725d43a1e683e68a8cf4f36a7ac1
Scutaru Stefan <daniel.stefan02@yahoo.com>
2025-09-20 21:55:29 +0300

    scrapping suprafara, etaj and an_constructie

--- Scraping/scraper OLX.py
+++ Scraping/scraper OLX.py
@@ -16,23 +16,45 @@ for link in links:
     try:
         response = requests.get(link)
         soup = BeautifulSoup(response.text, 'lxml')
-    
-        judete = soup.find_all('p', class_='css-z0m36u')
-        judet = judete[1].get_text(strip=True) if len(judete) > 1 else None
-    
-        oras = soup.find('p', class_='css-7wnksb').text.strip()
         
-        pret = soup.find('h3', class_='css-1m6jpd2').text.strip()
+        oras = soup.find('p', class_='css-9pna1a').text.strip()
+        
+        judet = soup.find("p", class_="3cz5o2")
+        elemente = soup.find_all('p', class_='css-13x8d99')
+
+        # Initialize variables to hold the extracted data
+        suprafata = None
+        etaj = None
+        an_constructie = None
+        compartimentare = None
+        for element in elemente:
+            text = element.text.strip()
+    
+            if text.startswith('Suprafata utila'):
+                suprafata = text.split(':',1)[1].strip()
+            elif text.startswith('Etaj'):
+                etaj = text.split(':',1)[1].strip()
+            elif text.startswith('An constructie'):
+                an_constructie = text.split(':',1)[1].strip()
+            elif text.startswith('Compartimentare'):
+                compartimentare = text.split(':',1)[1].strip()
+        #an_constructie = elemente.pop(3).text.strip()
+
+        pret = soup.find('h3', class_='css-1j840l6').text.strip()
     
     except Exception as e:
         print(f"An error occurred: {e}")
         continue
     
     rezultate.append({
-        'oras': oras,
         'judet': judet,
-        'pret': pret,})
-
+        'oras': oras,
+        'suprafata': suprafata,
+        'etaj': etaj,
+        'an_constructie': an_constructie,
+        'pret': pret})
+    print(judet, oras, suprafata, etaj, pret)
+    
 df = pd.DataFrame(rezultate)
 df.to_csv('olx_imobiliare.csv' , index=False, encoding='utf-8-sig')
 print("Scraping completed and data saved to olx_imobiliare.csv")
